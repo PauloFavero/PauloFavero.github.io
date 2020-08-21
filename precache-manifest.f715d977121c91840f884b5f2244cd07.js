b1a4bcf7aeac7bf72c9b4d7934c419404ca18271
Paulo Pereira <>
2020-08-21 00:27:25 -0300

    Updates

---
+++ precache-manifest.f715d977121c91840f884b5f2244cd07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b6a65552127ab7f42817fb4ba11bffd",
+    "url": "/webpage/index.html"
+  },
+  {
+    "revision": "4814c6e4a50f62504d0c",
+    "url": "/webpage/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1bc3e5a35a79d1931258",
+    "url": "/webpage/static/js/2.a1cedbb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webpage/static/js/2.a1cedbb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4814c6e4a50f62504d0c",
+    "url": "/webpage/static/js/main.e7bb26a9.chunk.js"
+  },
+  {
+    "revision": "318a061e1d2c2f3bda83",
+    "url": "/webpage/static/js/runtime-main.adf0b9a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webpage/static/media/logo.5d5d9eef.svg"
+  }
+]);
